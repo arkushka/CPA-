95fffe2cf8b50ffcb30416004a8d71a5e9ba5a48
Yulia <32716680+arkushka@users.noreply.github.com>
2017-10-22 22:22:43 +0300

    Add some labs

---
+++ Chapter 1/Lab_1_1_2/Lab_1_1_2/Lab112.cpp
@@ -0,0 +1,12 @@
+// Lab112.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main(void) {
+	cout << "Hello, my dear friend!" << endl << "Hello, friend of my friend!\n";
+	cout << "It's me, your first program.\n"; 
+	return 0;
+}
---
+++ Chapter 1/Lab_1_1_3/Lab_1_1_3/Lab113.cpp
@@ -0,0 +1,18 @@
+// Lab113.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+	int threeHoursInSeconds = 10800; 
+	int threeMinutesInSeconds = 3 * 60;
+	int fiveMinutesInSeconds = 5 * 60;
+	float valueOfPi = 3.141526; 
+	std::cout << "result: " << threeHoursInSeconds << std::endl;
+}
+
