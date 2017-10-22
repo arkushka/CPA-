5490b93bf9b2c335b599a6170961e4c632753d8d
Yulia <32716680+arkushka@users.noreply.github.com>
2017-10-23 00:05:35 +0300

    Added one more lab.

---
+++ Chapter 1/Lab_1_1_4_2/Lab_1_1_4_2/Lab1142.cpp
@@ -0,0 +1,24 @@
+// Lab1142.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main(void) {
+	float pi = 3.14159265359;
+	float x, y, firstPart, secondPart;
+	float squaredX;
+	float squaredPi = pi * pi;
+	cout << "Enter value for x: ";
+	cin >> x;
+	squaredX = x * x;
+
+	firstPart = squaredX / (squaredPi * (squaredX + (1.0 / 2.0)));
+	secondPart = 1 + ((squaredX) / ((squaredPi) * (squaredX - (1.0 / 2.0)) * (squaredX - (1.0 / 2.0))));
+	y = firstPart * secondPart;
+
+	cout << "y = " << y << endl;
+	return 0;
+}
+
