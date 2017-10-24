8319c59e7e46d5fbab51afed3e9c69ef50289714
Yulia <32716680+arkushka@users.noreply.github.com>
2017-10-24 19:09:14 +0300

    Fixed 2-nd lab of 2-1 Labs

--- Chapter 2/Lab_2_1_2/Lab_2_1_2/Lab212.cpp
+++ Chapter 2/Lab_2_1_2/Lab_2_1_2/Lab212.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
-int main()
-{
-    return 0;
-}
+int main(void) {
+	float grossprice, taxrate, netprice, taxvalue;
+	do {
+		cout << "Enter a gross price: "; // валова ціна
+		cin >> grossprice;
+	} while (grossprice < 0);
+	do {
+		cout << "Enter a tax rate: "; // ставка податку
+		cin >> taxrate;
+	} while (taxrate < 0 || taxrate > 100);
 
+	//taxvalue = grossprice - netprice;
+	//taxvalue = netprice*taxrate / 100;
+	netprice = 100*grossprice / (taxrate + 100);
+	taxvalue = grossprice - netprice;
+
+	cout << "Net price: " << netprice << endl; // фактична ціна 
+	cout << "Tax value: " << taxvalue << endl; // вартість податку
+	return 0;
+}
