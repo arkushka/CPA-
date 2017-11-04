2aa5e7a06cc154f6fa6d4a4e7cc1b4101bd2e197
Yulia <32716680+arkushka@users.noreply.github.com>
2017-11-04 16:52:16 +0200

    Added 4-th lab from 2-9 Labs

---
+++ Chapter 2/Lab_2_9_4/Lab_2_9_4/Lab294.cpp
@@ -0,0 +1,35 @@
+// Lab294.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+	double vector[] = { 1., 2., 3., 4., 5. };
+	int n = sizeof(vector) / sizeof(vector[0]);
+	double ArithmeticMean = 0;
+	double HarmonicMean = 0;
+	double GeometricMean = 1;
+	double RootMeanSquare = 0;
+
+	for (int i = 0; i < n; i++) {
+		ArithmeticMean += vector[i] / n;
+		HarmonicMean += 1 / vector[i];
+		GeometricMean *= vector[i];
+		RootMeanSquare += pow(vector[i], 2);
+	}
+	HarmonicMean = n / HarmonicMean;
+	GeometricMean = pow(GeometricMean, 1. / n);
+	RootMeanSquare = sqrt(RootMeanSquare / n);
+
+	cout << "Arithmetic Mean = " << ArithmeticMean << endl;
+	cout << "Harmonic Mean = " << HarmonicMean << endl;
+	cout << "Geometric Mean = " << GeometricMean << endl;
+	cout << "RootMean Square = " << RootMeanSquare << endl;
+	cout << endl;
+	return 0;
+}
+
