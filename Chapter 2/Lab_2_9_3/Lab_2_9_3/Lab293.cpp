dad6a2f526cc8b7759f76a5274cf29684d13e2a5
Yulia <32716680+arkushka@users.noreply.github.com>
2017-11-04 14:14:29 +0200

    Added 3-d lab from 2-9 Labs

---
+++ Chapter 2/Lab_2_9_3/Lab_2_9_3/Lab293.cpp
@@ -0,0 +1,27 @@
+// Lab293.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int vector[] = { 1, 7, 3, 8, 3, 7, 1 };
+	bool ispalindrome = true;
+	int n = sizeof(vector) / sizeof(vector[0]);
+
+	for (int i = 0; i < n; i++) {
+		if (vector[i] != vector[n - i - 1])
+		{
+			ispalindrome = false;
+		}
+	}
+
+	if (ispalindrome)
+		cout << "It's a palindrome";
+	else
+		cout << "It isn't a palindrome";
+	cout << endl;
+	return 0;}
+
