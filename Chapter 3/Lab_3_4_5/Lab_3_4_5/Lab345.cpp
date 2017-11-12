dcb844ab510809db8ce5eebd417b2aa42322c6cd
Yulia <32716680+arkushka@users.noreply.github.com>
2017-11-12 22:52:43 +0200

    Added labs from 3-4 and 3-6 Labs

---
+++ Chapter 3/Lab_3_4_5/Lab_3_4_5/Lab345.cpp
@@ -0,0 +1,29 @@
+// Lab345.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+struct Date {
+	int year;
+	int month;
+	int day;
+};
+
+Date today(void) {
+	time_t timeInSec = time(NULL);
+	tm convertedTime = *localtime(&timeInSec);
+	Date date = { convertedTime.tm_year + 1900, convertedTime.tm_mon + 1, convertedTime.tm_mday };
+
+	return date;
+}
+
+int main(void) {
+	Date t = today();
+	cout << t.year << "-" << t.month << "-" << t.day << endl;
+	return 0;
+}
+
---
+++ Chapter 3/Lab_3_6/Lab_3_6/Lab36.cpp
@@ -0,0 +1,23 @@
+// Lab36.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void increment(int &var, int exp = 1) {
+	var += exp;
+}
+
+int main(void) {
+	int var = 0;
+	for (int i = 0; i < 10; i++)
+		if (i % 2)
+			increment(var);
+		else
+			increment(var, i);
+	cout << var << endl;
+	return 0;
+}
+
