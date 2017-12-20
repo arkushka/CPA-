c2fbefd3888c278a5c66d640be5bf210241aa5b3
Yulia <32716680+arkushka@users.noreply.github.com>
2017-12-20 13:09:01 +0200

    for checking

---
+++ Chapter 2/Lab_2_3_6/Lab_2_3_6/Lab236.cpp
@@ -0,0 +1,19 @@
+// Lab236.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int height;
+	cout << "Enter a height oh pyramids (2 < height < 9): ";
+	cin >> height;
+
+
+
+	return 0;
+}
+
